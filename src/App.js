c1362be0177039b657089c38e0b0f5c3da1440e5
Gabriel Lamas <gabrielldarts@gmail.comm>
2020-08-02 17:28:48 -0300

    Implement h1 and header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Logo from './img/training-play-logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <h1>Oi Brasil</h1>
+      <header>
+        <img src={Logo} alt="Training Play | Escolha sua serie de treinos"/>
+        <button className="btn btn-primary btn-line" type="button">Entrar</button>
+      </header>
+
+      <section>
+        <div className="container">
+          <h1>Encontre a melhor serie de exercícios para maratonar</h1>
+          <button className="btn btn-primary btn-large" type="button">Assine agora</button>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default App;
-
-//https://colorhunt.co/palette/144399
-//Paleta
